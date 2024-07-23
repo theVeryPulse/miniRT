cfb92ee2b36cdc867787c603bbe373f11a97c27b
Philip <macpilot23@outlook.com>
2024-07-23 13:41:08 +0100

    feat (vector): add cross product function

--- src/geometry/vector/vector.h
+++ src/geometry/vector/vector.h
@@ -6,7 +6,7 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 22:32:03 by Philip            #+#    #+#             */
-/*   Updated: 2024/07/15 17:41:11 by Philip           ###   ########.fr       */
+/*   Updated: 2024/07/23 13:40:11 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ extern t_vector	vec_div(t_vector a, double t);
 extern double	vec_dot(t_vector a, t_vector b);
 extern t_vector	vec_minus(t_vector a, t_vector b);
 extern t_vector	vec_mult(double t, t_vector a);
+extern t_vector	vec_cross(t_vector a, t_vector b);
 
 extern double	vec_len(t_vector vec);
 extern t_vector	vec_normalized(t_vector vec);
