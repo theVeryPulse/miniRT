bc5734410fc89c0b31098f3c232ce312cde4da8c
Philip <macpilot23@outlook.com>
2024-07-23 19:47:50 +0100

    feat (vec_div): add 0 division check

--- src/geometry/vector/vec_div.c
+++ src/geometry/vector/vec_div.c
@@ -6,13 +6,19 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 22:35:33 by Philip            #+#    #+#             */
-/*   Updated: 2024/07/12 16:19:16 by Philip           ###   ########.fr       */
+/*   Updated: 2024/07/23 19:44:57 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.h"
+#include "stdio.h"
 
 extern t_vector	vec_div(t_vector a, double t)
 {
+	if (t == 0)
+	{
+		printf("Warning[vec_div]: zero division");
+		return ((t_point){0});
+	}
 	return (vec_mult(1 / t, a));
 }
