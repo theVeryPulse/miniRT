87749513c9c5dae6b55a1704d63f6e2e603cdfb4
Philip <macpilot23@outlook.com>
2024-07-26 00:06:31 +0100

    feat (read file): open and read all content of file

--- src/read_file.c
+++ src/read_file.c
@@ -6,13 +6,16 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 22:34:24 by Philip            #+#    #+#             */
-/*   Updated: 2024/07/25 23:44:36 by Philip           ###   ########.fr       */
+/*   Updated: 2024/07/26 00:05:16 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/libft/inc/libft.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define TEST 1
 
@@ -42,12 +45,38 @@ void	check_filename(const char *filename)
 	}
 }
 
+void	load_scene(const char* filename)
+{
+	int		file;
+	char	*line;
+	t_list	*all_lines;
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		write(STDOUT_FILENO, "Error: cannot open ", 20);
+		perror(filename);
+		exit(1);
+	}
+	all_lines = NULL;
+	line = get_next_line(file);
+	while (line)
+	{
+		printf("%s", line); /* Test */
+		ft_lstadd_back(&all_lines, ft_lstnew(line));
+		line = get_next_line(file);
+	}
+
+	ft_lstclear(&all_lines, free);
+}
+
 #if TEST
 
 int	main(int argc, const char **argv)
 {
 	check_argc(argc);
 	check_filename(argv[1]);
+	load_scene(argv[1]);
 	return (0);
 }
 
