3262feac2d2e5b086f64215cf516f3a6fd19989e
Philip <macpilot23@outlook.com>
2024-07-31 21:03:59 +0100

    backup (objects): function declarations before norm

---
+++ src/object/inc/object_backup.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   object_backup.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 20:27:16 by Philip            #+#    #+#             */
+/*   Updated: 2024/07/31 21:02:35 by Philip           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Backup 
+
+// #include "../t_object.h"
+
+// extern t_object	colored_sphere(
+// 					t_argb color,
+// 					t_raw_point position,
+// 					double radius,
+// 					double specular_exponent,
+// 					double reflectivity);
+
+// extern t_object	disk(t_argb color,
+// 					t_raw_point position,
+// 					t_vector direction,
+// 					double radius,
+// 					double specular_exponent,
+// 					double reflectivity);
+
+// extern t_object	plane(t_argb color,
+// 					t_raw_point position,
+// 					t_vector direction,
+// 					double specular_exponent,
+// 					double reflectivity);
+
+// extern t_object	cylinder(
+// 					t_argb color,
+// 					t_raw_point position,
+// 					t_vector direction,
+// 					double radius,
+// 					double height,
+// 					double specular_exponent,
+// 					double reflectivity);
+
+// extern t_object	point_light(t_raw_point position, double intensity);
+
+// extern t_object	ambient_light(double intensity);
+
+// extern t_object	directional_light(double intensity, t_vector direction);
