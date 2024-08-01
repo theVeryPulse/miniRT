38b4f6e7479ac5bf0c31340f2f774239ae6d279c
Philip <macpilot23@outlook.com>
2024-08-01 15:35:44 +0100

    refactor (key press): norm

--- src/key_press/handle_keypress_event.c
+++ src/key_press/handle_keypress_event.c
@@ -6,7 +6,7 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 14:21:20 by Philip            #+#    #+#             */
-/*   Updated: 2024/08/01 15:16:10 by Philip           ###   ########.fr       */
+/*   Updated: 2024/08/01 15:34:32 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,55 @@
 #include <stdio.h>
 #include <stdlib.h> /* free */
 
-void	put_image_to_window_vars(t_vars *vars);
-int		clean_exit(int exit_status);
-void	render_image(t_vars *vars);
+/* Functions defined in other files */
+
+extern void	put_image_to_window_vars(t_vars *vars);
+extern int	clean_exit(int exit_status);
+extern void	render_image(t_vars *vars);
+
+/* Functions defined in this file */
+
+extern int	handle_keypress_event(int key, t_vars *vars);
+static void	adjust_directional_light(t_object *focus, int key);
+static void	move_position(t_object *focus, int key);
+static void	adjust_light_intensity(t_object *light, int key);
+static void	change_fov(int key, t_vars *vars);
+
+/**
+ * @brief 
+ * 
+ * @param key 
+ * @param vars 
+ * @return int 
+ */
+extern int	handle_keypress_event(int key, t_vars *vars)
+{
+	t_object	*focus;
+
+	focus = vars->scene.focus;
+	printf("%d pressed\n", key);
+	if (key == XK_Escape)
+		clean_exit(0);
+	else if (key == XK_Tab)
+		switch_focus(vars);
+	else if (key == XK_Up || key == XK_Down || key == XK_Left || key == XK_Right
+		|| key == XK_i || key == XK_o
+		|| key == XK_Page_Up || key == XK_Page_Down)
+	{
+		if (focus->type == DirectionalLight)
+			adjust_directional_light(focus, key);
+		else
+			move_position(focus, key);
+		if (focus->category == Light
+			&& (key == XK_Page_Up || key == XK_Page_Down))
+			adjust_light_intensity(focus, key);
+		render_image(vars);
+		put_image_to_window_vars(vars);
+	}
+	else if (key == XK_bracketleft || key == XK_bracketright)
+		change_fov(key, vars);
+	return (0);
+}
 
 static void	adjust_directional_light(t_object *focus, int key)
 {
@@ -70,43 +116,20 @@ static void	adjust_light_intensity(t_object *light, int key)
 		light->intensity = 1;
 }
 
-extern int	handle_keypress_event(int key, t_vars *vars)
+static void	change_fov(int key, t_vars *vars)
 {
-	t_object	*focus;
+	char	*fov;
+	char	*message;
 
-	focus = vars->scene.focus;
-	printf("%d pressed\n", key);
-	if (key == XK_Escape)
-		clean_exit(0);
-	else if (key == XK_Tab)
-		switch_focus(vars);
-	else if (key == XK_Up || key == XK_Down || key == XK_Left || key == XK_Right
-		|| key == XK_i || key == XK_o
-		|| key == XK_Page_Up || key == XK_Page_Down)
-	{
-		if (focus->type == DirectionalLight)
-			adjust_directional_light(focus, key);
-		else
-			move_position(focus, key);
-		if (focus->category == Light
-			&& (key == XK_Page_Up || key == XK_Page_Down))
-			adjust_light_intensity(focus, key);
-		render_image(vars);
-		put_image_to_window_vars(vars);
-	}
-	else if (key == XK_bracketleft || key == XK_bracketright)
-	{
-		if (key == XK_bracketleft)
-			update_fov(minirt()->fov - 1);
-		else if (key == XK_bracketright)
-			update_fov(minirt()->fov + 1);
-		render_image(vars);
-		put_image_to_window_vars(vars);
-		char *fov = ft_itoa((int)minirt()->fov);
-		char *message = ft_format_string("FOV: %s");
-		mlx_string_put(vars->mlx_ptr, vars->win_ptr, 10, 10, GREEN, message);
-		free(fov);
-		free(message);
-	}
-	return (0);
+	if (key == XK_bracketleft)
+		update_fov(minirt()->fov - 1);
+	else if (key == XK_bracketright)
+		update_fov(minirt()->fov + 1);
+	render_image(vars);
+	put_image_to_window_vars(vars);
+	fov = ft_itoa((int)minirt()->fov);
+	message = ft_format_string("FOV: %s");
+	mlx_string_put(vars->mlx_ptr, vars->win_ptr, 10, 10, GREEN, message);
+	free(fov);
+	free(message);
 }
