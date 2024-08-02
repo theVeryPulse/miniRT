dcb780ab7ce85e2c1a8115618d54ab2ca17a116a
Philip <macpilot23@outlook.com>
2024-08-02 18:15:41 +0100

    refactor (shader): put `get_local_color` in a function

--- src/shader/shade.c
+++ src/shader/shade.c
@@ -6,7 +6,7 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 02:41:38 by Philip            #+#    #+#             */
-/*   Updated: 2024/08/02 16:10:09 by Philip           ###   ########.fr       */
+/*   Updated: 2024/08/02 18:14:44 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,21 @@
 #include <math.h>
 
 extern t_argb	get_checkerboard_sphere_color(t_point pt, t_argb color1,
-				t_argb color2);
+					t_argb color2);
+
+static t_argb	get_local_color(t_object *object, t_point intersection,
+			double intensity)
+{
+	t_argb	local_color;
+
+	if (object->is_checkerboard && object->type == Sphere)
+		local_color = get_checkerboard_sphere_color(
+				vec_minus(intersection, object->position), WHITE, BLACK);
+	else
+		local_color = object->color;
+	local_color = color_mult(local_color, intensity);
+	return (local_color);
+}
 
 t_argb	shade(t_scene *scene, t_ray *ray, t_closest *closest,
 			uint8_t recursion_depth)
@@ -35,21 +49,9 @@ t_argb	shade(t_scene *scene, t_ray *ray, t_closest *closest,
 	intersection = vec_add(ray->origin, vec_mult(closest->t, ray->direction));
 	unit_normal = normal_on_surface(closest->object, intersection);
 	intensity = calculate_light_intensity(scene, intersection, unit_normal,
-		vec_mult(-1, ray->direction), closest->object->specular_exponent);
-	if (closest->object->is_checkerboard)
-		local_color = color_mult(
-			get_checkerboard_sphere_color(
-				vec_minus(intersection, closest->object->position),
-				WHITE,
-				BLACK
-			),
-			intensity
-		);
-	else
-		local_color = color_mult(closest->object->color, intensity);
-
+			vec_mult(-1, ray->direction), closest->object->specular_exponent);
+	local_color = get_local_color(closest->object, intersection, intensity);
 	// return (local_color); /* Return color here to skip reflection */
-
 	/* When recursion limit is hit or the other object does not reflect */
 	if (recursion_depth <= 0 || closest->object->reflectivity <= 0)
 		return (local_color);
