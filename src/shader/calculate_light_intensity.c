4de1446b40cd83283a4fe4551e4cb1f99503648e
Philip <macpilot23@outlook.com>
2024-08-02 16:24:53 +0100

    refactor (light intensity): use pointer rather than indexing

--- src/shader/calculate_light_intensity.c
+++ src/shader/calculate_light_intensity.c
@@ -6,7 +6,7 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 16:06:08 by Philip            #+#    #+#             */
-/*   Updated: 2024/08/02 16:10:56 by Philip           ###   ########.fr       */
+/*   Updated: 2024/08/02 16:23:42 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,33 +34,32 @@
 double	calculate_light_intensity(t_scene *scene, t_point point, t_vector normal,
 		t_vector view, double specular_exponent)
 {
-	size_t		i;
+	t_object	*light;
 	double		intensity;
-	t_vector	light;
+	t_vector	light_vec;
 	double		normal_dot_light;
 	double		t_max;
 
 	intensity = 0.0;
-	i = 0;
-	while (i < scene->light_count)
+	light = scene->lights;
+	t_max = INFINITY;
+	while (light < scene->light_count + scene->lights)
 	{
-		if (scene->lights[i].type == AmbientLight)
-			intensity += scene->lights[i].intensity;
+		if (light->type == AmbientLight)
+			intensity += light->intensity;
 		else
 		{
-			if (scene->lights[i].type == PointLight)
+			if (light->type == PointLight)
 			{
-				light = vec_minus(scene->lights[i].position, point);
-				t_max = vec_len(light);
-				light = vec_normalized(light);
+				light_vec = vec_minus(light->position, point);
+				t_max = vec_len(light_vec);
+				light_vec = vec_normalized(light_vec);
 			}
-			else if (scene->lights[i].type == DirectionalLight)
+			else if (light->type == DirectionalLight)
 			{
-				light = scene->lights[i].direction;
+				light_vec = light->direction;
 				t_max = INFINITY;
 			}
-			else /* Silencing gcc warning */
-				t_max = INFINITY;
 
 			/* Shadow check */
 			t_ray		shadow_ray;
@@ -69,20 +68,20 @@ double	calculate_light_intensity(t_scene *scene, t_point point, t_vector normal,
 			closest.object = NULL;
 			closest.t = INFINITY;
 			shadow_ray.origin = point;
-			shadow_ray.direction = light;
+			shadow_ray.direction = light_vec;
 			shadow_ray.t_min = 1e-4;
 			shadow_ray.t_max = t_max;
 			if (trace(scene, &shadow_ray, &closest))
 			{
-				++i;
+				++light;
 				continue;
 			}
 
 			/* Diffuse reflection */
-			normal_dot_light = vec_dot(normal, light);
+			normal_dot_light = vec_dot(normal, light_vec);
 			if (normal_dot_light > 0)
-				intensity += scene->lights[i].intensity * normal_dot_light
-					/ (vec_len(normal) * vec_len(light));
+				intensity += light->intensity * normal_dot_light
+					/ (vec_len(normal) * vec_len(light_vec));
 			
 			/* Specular reflection */
 			if (specular_exponent > 0.0)
@@ -90,18 +89,18 @@ double	calculate_light_intensity(t_scene *scene, t_point point, t_vector normal,
 				t_vector	reflection;
 				double		reflection_dot_view;
 
-				reflection = reflect_ray(light, normal);
+				reflection = reflect_ray(light_vec, normal);
 				reflection_dot_view = vec_dot(reflection, view);
 				if (reflection_dot_view > 0)
 				{
-					intensity += scene->lights[i].intensity
+					intensity += light->intensity
 						* pow(reflection_dot_view
 						/ (vec_len(reflection) * vec_len(view)),
 						specular_exponent);
 				}
 			}
 		}
-		++i;
+		++light;
 	}
 	if (intensity >= 1)
 		intensity = 1;
