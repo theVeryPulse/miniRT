5dad8a47376ba2c58e18171aee5d614a2d784584
Philip <macpilot23@outlook.com>
2024-08-02 21:01:30 +0100

    refactor (load scene from function): remove unused codes, adjust indent

--- src/main.c
+++ src/main.c
@@ -192,22 +192,14 @@ void	load_default_scene(t_scene *scene)
 	scene->camera = camera((t_point){0, 0, 0}, (t_vector){0, 0, 1});
 	unsigned int	i = 11;
 	allocate_objects(scene, i);
-	// scene->objects[--i] = checkerboard_sphere(
-	// 	(t_point){10, -150, -2000}, 200.0, 10.0, 0.2);
-	// scene->objects[--i] = checkerboard_sphere(
-	// 	(t_raw_point){0, -0.02, -2}, 0.33, 10.0, 0.2);
-
 	scene->objects[--i] = checkerboard_sphere(
 		(t_cs){(t_raw_point){0, -0.02, -2}, 0.33, 10.0, 0.2});
-		
 	scene->objects[--i] = colored_sphere((t_s){
 		RED, (t_raw_point){200.0/960.0, 200.0/960.0, -2500.0/960.0},
 		300.0/960.0, 5.0, 0.1});
-
 	scene->objects[--i] = colored_sphere((t_s){
 		RED, (t_raw_point){200.0/960.0, 200.0/960.0, -2500.0/960.0},
 		300.0/960.0, 5.0, 0.1});
-	
 	scene->objects[--i] = colored_sphere((t_s){
 		YELLOW, (t_raw_point){-700.0/960, -200.0/960.0, -2500.0/960.0},
 		300.0/960.0, 1000.0, 0.5});
@@ -215,6 +207,7 @@ void	load_default_scene(t_scene *scene)
 	scene->objects[--i] = plane((t_p){
 		BLUE, (t_raw_point){-960.0/960.0, 0, 0},
 		(t_vector){-1, 0, 0}, 100.0, 0.1});
+	// Right wall
 	scene->objects[--i] = plane((t_p){
 		CYAN, (t_raw_point){960.0/960.0, 0, 0}, (t_vector){1, 0, 0}, 10.0, 0.0});
 	// Ceiling
@@ -257,14 +250,16 @@ void	load_test_scene(t_scene *scene)
 	allocate_objects(scene, object_count);
 
 	// Wall in back
-	scene->objects[--object_count] = plane((t_p){WHITE, (t_point){0.0/960.0, -100.0/960.0, -2000.0/960.0},
+	scene->objects[--object_count] = plane((t_p){WHITE,
+		(t_point){0.0/960.0, -100.0/960.0, -2000.0/960.0},
 		(t_vector){0, 0, 1}, 10.0, 0.0});
 	// scene->objects[--object_count] = checkerboard_sphere(
 	// 	(t_raw_point){0.01, 0.02, -2}, 0.5, 100, 0.0);
 	scene->objects[--object_count] = cylinder((t_c){RED,
-		(t_point){10.0/960.0, 10.0/960.0, -1500.0/960.0},
+		(t_point){0/960.0, 0/960.0, -1500.0/960.0},
 		(t_vector){0, 0, 1}, 200.0/960.0, 200.0/960.0, 1.0, 0.0});
-	// scene->objects[--object_count] = disk(RED, (t_point){400.0/960.0, 0, -1500.0/960.0},
+	// scene->objects[--object_count] = disk(RED,
+		// (t_point){400.0/960.0, 0, -1500.0/960.0},
 	// 	(t_vector){0, 0.1, -1}, 200.0/960.0, 1.0, 0.0);
 
 	unsigned int	light_count;
